8f110c13b2eae4676127cc5fcc38acf2fdef84ee
adam <git.sucks.this.email.doesnt.exist@mailinator.com>
2011-03-13 20:34:24 +0000

    Added missing System header

---
+++ src/EntitySystem/System.h
@@ -0,0 +1,14 @@
+#import <UIKit/UIKit.h>
+
+/**
+ * Standard design: c.f. http://entity-systems.wikidot.com/rdbms-with-code-in-systems
+ */
+
+@interface System : NSObject
+{
+
+}
+
+-(void) processOneGameTick;
+
+@end
