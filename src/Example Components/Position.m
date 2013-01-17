2a3c0728a3abfc34910b3d0f5f514b87ef96c791
Patrick McKenna <patrick.a.mckenna@gmail.com>
2013-01-16 21:00:43 -0500

    Add a definition for the type property of the Position component

--- src/Example Components/Position.m
+++ src/Example Components/Position.m
@@ -5,4 +5,18 @@
 
 @synthesize x, y;
 
+- (id)init
+{
+	self = [super init];
+	if (self) {
+		[self initialize];
+	}
+	return self;
+}
+
+-(void) initialize
+{
+	self.type = ComponentTypePosition;
+}
+
 @end
