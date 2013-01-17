7264e0233d9f95ee158e0d1725c156d83aa35e61
Patrick McKenna <patrick.a.mckenna@gmail.com>
2013-01-16 21:04:02 -0500

    Add example System: MovementSystem; accesses Position component

---
+++ src/Example Systems/MovementSystem.h
@@ -0,0 +1,13 @@
+//
+//  MovementSystem.h
+//  PortPortProot
+//
+//  Created by Patrick McKenna on 13-01-16.
+//  Copyright (c) 2013 Patrick McKenna. All rights reserved.
+//
+
+#import "System.h"
+
+@interface MovementSystem : System
+
+@end
